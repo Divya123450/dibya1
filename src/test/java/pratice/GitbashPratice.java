3d6f0dcc28981c83d4382b43c38fb955e668ad15
Divya123450 <sahoodivyalochan19@gmail.com>
2022-07-25 15:59:30 +0530

    hiiiiii

---
+++ src/test/java/pratice/GitbashPratice.java
@@ -0,0 +1,12 @@
+package pratice;
+
+public class GitbashPratice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hiiiii");
+System.out.println("helloo");
+System.out.println("bye");
+	}
+
+}
